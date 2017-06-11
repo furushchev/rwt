fd0c321b9f14c6d31dfcd9e69cf43bde6d1bdf64
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-06-11 22:27:29 +0900

    add status

--- speech_recognition/index.js
+++ speech_recognition/index.js
@@ -1,34 +1,52 @@
 $(document).ready(function() {
   var _sr = null;
-  var start_recognize = function() {
+  var _result = null;
+  var _status = null;
+  function start_recognize(lang) {
     var sr = new webkitSpeechRecognition();
     sr.continuous = true;
     sr.interimResults = false;
-    sr.lang = 'ja-JP';
+    sr.lang = lang || 'ja-JP';
     sr.onresult = function(e) {
       for (var i = e.resultIndex; i < e.results.length; ++i) {
         if (e.results[i].isFinal) {
+          _result = e.results[i];
           var str = e.results[i][0].transcript;
           console.log('Recognized: ' + str);
-          $("#text").text(str);
+          $("#result").text(str);
           start_recognize();
         }
       }
     };
-    sr.onerror = function() {
-      console.log("onerror");
-      start_recognize();
+    sr.onnomatch = function() {
+      console.log("onnomatch");
+      _status = "nomatch";
+    }
+    sr.onerror = function(e) {
+      var err = "onerror: " + e.message;
+      console.log(err);
+      _status = err;
+      // start_recognize();
+    };
+    sr.onsoundstart = function() {
+      console.log("onsoundstart");
+      _status = "onsoundstart";
     };
     sr.onsoundend = function() {
       console.log("onsoundend");
-      start_recognize();
+      _status = "onsoundend";
+      //      start_recognize();
     };
     sr.start();
     _sr = sr;
   };
 
-  var stop_recognize = function() {
+  function stop_recognize() {
     if (_sr !== null) _sr.stop();
+  };
+
+  function get_status() {
+    return _status;
   };
 
   $("#start-button").click(start_recognize);
